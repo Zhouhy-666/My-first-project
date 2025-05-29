b237e9aa4899d648e2aec1768b7d2e39929972a2
Zhou Hao Yu <482209895@qq.com>
2025-05-29 20:55:24 +0800

    Add files via upload

---
+++ Hello world!.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    cout << "Hello world!" << '\n';
+}
+
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
